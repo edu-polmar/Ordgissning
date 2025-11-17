8dc380e14540d2150044219adc5394f2646233ad
edu-polmar <polina.martynenko@skola.taby.se>
2025-11-17 14:49:56 +0100

    rename base

--- gissningsspel.py
+++
@@ -1,6 +0,0 @@
-import random
-def Hemligt():
-    with open("Ordlista.txt", "r", encoding="utf-8") as fil:
-        Ord = fil.read().splitlines()
-    slump_ord = random.choice(Ord)
-    return slump_ord
