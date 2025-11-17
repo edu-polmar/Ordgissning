89b473b030e59e0d4c9e211e6fa737443f1e51c9
edu-fricol <frida.collnyberg@skola.taby.se>
2025-11-17 14:40:50 +0100

    Update gissningsspel.py

--- gissningsspel.py
+++ gissningsspel.py
@@ -4,4 +4,15 @@ def Hemligt():
         Ord = fil.read().splitlines()
     slump_ord = random.choice(Ord)
     return slump_ord
-print (Hemligt())
+
+
+hemligt_ord = Hemligt() #Hämtar slumpat ord från ordlistan (filen)
+gissning = ["_"] * len(hemligt_ord)
+print("Det hemliga ordet har", len(hemligt_ord),"bokstäver.")
+
+
+#I slutet av spelet, om man gissat rätt ord, eller förlorat alla gissningar
+if "_" not in gissning:
+    print("Grattis! Du gissade rätt ord:", hemligt_ord)
+else: 
+    print("Du förlorade! Ordet var:", hemligt_ord)
